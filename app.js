55b90183e17a336a022c415eed1b7e3c5c4da459
1Keiny1 <keinyysessy@gmail.com>
2025-10-07 19:21:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -383,3 +383,6 @@ app.post('/modificarUsuario', validarNombre, (req, res) => {
         }
     );
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
