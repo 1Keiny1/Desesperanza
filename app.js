b2633c7b71cf2e89b896ed798cc1f7d431f45e2f
1Keiny1 <keinyysessy@gmail.com>
2025-10-07 18:46:21 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var con=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'n0m3l0',
-    database:'desesperanza'
+    database:'conexion'
 })
 con.connect();
 
@@ -15,28 +15,104 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
+function validarNombre(req, res, next) {
+    const nombre = removeTags(req.body.nombre || req.body.nombre_b || req.body.nombre_ant || req.body.nombre_nuevo);
+    if (!nombre || numeroRegex.test(nombre)) {
+        return res.send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Error</h1>
+                                        <p>No se permiten números</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+        `);
+    }
+    req.nombreLimpio = nombre; // lo guardamos para usarlo en la query
+    next();
+}
+
+
+const numeroRegex = /\d/;
+
+function removeTags(html) {
+    if (!html) return '';
+    return html.replace(/<[^>]*>?/gm, '').replace(/<\?php.*?\?>/gs, '');
+    }
+
 app.use(express.static('public'))
 
-app.post('/agregarUsuario',(req,res)=>{
-        let nombre=req.body.nombre
-        let id=req.body.id
+app.post('/agregarUsuario', validarNombre, (req, res) => {
+    let nombre =  removeTags(req.body.nombre);
 
-        con.query('INSERT INTO usuario (id_usuario, nombre) VALUES (?, ?)', [id, nombre], (err, respuesta, fields) => {
+    con.query(
+        'INSERT INTO usuario (nombre) VALUES (?)',
+        [nombre],
+        (err, respuesta, fields) => {
             if (err) {
-                console.log("Error al conectar", err);
-                return res.status(500).send("Error al conectar");
+                console.log("Error al insertar", err);
+                return res.status(500).send("Error al insertar");
             }
-           
-            return res.send(`<h1>Nombre:</h1> ${nombre}`);
-        });
-   
-})
+            return res.send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario agregado</h1>
+                                        <p>Nombre: ${nombre}</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+            `);
+        }
+    );
+});
+
 
 app.listen(10000,()=>{
     console.log('Servidor escuchando en el puerto 10000')
 })
 
-//fun consultar
+//fin consultar
 
 
 app.get('/obtenerUsuario',(req,res)=>{
@@ -44,7 +120,7 @@ app.get('/obtenerUsuario',(req,res)=>{
         if(err)return console.log('ERROR: ', err);
         var userHTML=``;
         var i=0;
-
+        
         respuesta.forEach(user => {
             i++;
             userHTML+= `<tr><td>${i}</td><td>${user.nombre}</td></tr>`;
@@ -52,31 +128,258 @@ app.get('/obtenerUsuario',(req,res)=>{
 
         });
 
-        return res.send(`<table>
-                <tr>
-                    <th>id</th>
-                    <th>Nombre:</th>
-                <tr>
-                ${userHTML}
-                </table>`
-        );
+        return res.send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                    <h1 class="card-title text-center mb-4">Obtener Usuarios</h1>
+                                    <div class="table-responsive">
+                                        <table class="table table-striped table-hover align-middle">
+                                            <thead class="table-dark">
+                                                <tr>
+                                                    <th scope="col">ID</th>
+                                                    <th scope="col">Nombre</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                ${userHTML}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+        `);
 
 
     });
 });
 
-app.post('/borrarUsuario', (req, res) => {
-    const id = req.body.id; // El ID del usuario a eliminar viene en el cuerpo de la solicitud
-    console.log("hola")
-    con.query('DELETE FROM usuario WHERE id_usuario = ?', [id], (err, resultado, fields) => {
+app.post('/borrarUsuario', validarNombre, (req, res) => {
+    const nombre_b =  removeTags(req.body.nombre_b);
 
+    con.query('DELETE FROM usuario WHERE nombre = ?', [nombre_b], (err, resultado) => {
         if (err) {
             console.error('Error al borrar el usuario:', err);
-            return res.status(500).send("Error al borrar el usuario");
+            return res.status(500).send(`
+                 <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Eliminado</h1>
+                                        <p>"Error al borrar el usuario"</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+            `);
         }
+
         if (resultado.affectedRows === 0) {
-            return res.status(404).send("Usuario no encontrado");
+            return res.send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Eliminado</h1>
+                                        <p>No se encontró ningún usuario con el nombre "${nombre_b}"</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+
+                `);
         }
-        return res.send(`Usuario con ID ${id} borrado correctamente`);
+
+        return res.send(`
+        <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Eliminado</h1>
+                                        <p>Usuario "${nombre_b}" borrado correctamente</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+            `);
     });
+});
+
+app.post('/modificarUsuario', validarNombre, (req, res) => {
+    const nombre_ant =  removeTags(req.body.nombre_ant);
+    const nombre_nuevo =  removeTags(req.body.nombre_nuevo);
+
+    con.query(
+        'UPDATE usuario SET nombre = ? WHERE nombre = ?',
+        [nombre_nuevo, nombre_ant],
+        (err, resultado) => {
+            if (err) {
+                console.error('Error al modificar el usuario:', err);
+                return res.status(500).send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Modificado</h1>
+                                        <p>"Error al modificar el usuario"</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+                    
+                    `);
+            }
+
+            if (resultado.affectedRows === 0) {
+                return res.status(404).send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Modificado</h1>
+                                        <p>No se encontró ningún usuario con el nombre "${nombre_ant}"</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+                    
+                    `);
+            }
+
+            return res.send(`
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <title>Lista de Usuarios</title>
+            </head>
+            <body class="bg-light">
+                <div class="container py-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card shadow-lg border-0 rounded-4">
+                                <div class="card-body">
+                                        <h1>Usuario Modificado</h1>
+                                        <p>Usuario "${nombre_ant}" modificado correctamente a "${nombre_nuevo}"</p>
+                                    <div class="text-center mt-3">
+                                        <a class="btn btn-outline-secondary" href="/">Volver</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </body>
+            </html>
+                `);
+        }
+    );
 });
